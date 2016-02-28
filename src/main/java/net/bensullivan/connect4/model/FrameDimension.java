2cfc278682d644fff0804f560be256f89bae23fc
Ben Sullivan <office@bensullivan.net>
2016-02-28 18:32:42 +1000

    Replaced Cobertura with Jacoco for code coverage reporting

--- src/main/java/net/bensullivan/connect4/model/FrameDimension.java
+++ src/main/java/net/bensullivan/connect4/model/FrameDimension.java
@@ -9,31 +9,19 @@ public class FrameDimension {
         this.numColumns = numColumns;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        FrameDimension that = (FrameDimension) o;
-        return numRows == that.numRows && numColumns == that.numColumns;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = numRows;
-        result = 31 * result + numColumns;
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "FrameDimension{numRows=" + numRows + ", numColumns=" + numColumns + '}';
-    }
-
     public int getNumRows() {
         return numRows;
     }
 
     public int getNumColumns() {
         return numColumns;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FrameDimension that = (FrameDimension) o;
+        return numRows == that.numRows && numColumns == that.numColumns;
     }
 }
